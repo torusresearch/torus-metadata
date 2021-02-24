d2c0568c3f6c6f4b5f6b825a4d4ef257e05dd7b1
himanshu <himanshuchawla2014@gmail.com>
2021-02-24 16:40:19 +0530

    change note: prom client added instead of express prom bundle

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const cors = require("cors");
 const morgan = require("morgan");
 const log = require("loglevel");
 
-const promBundle = require("express-prom-bundle");
-
-const promMetrics = promBundle({ includePath: true });
+const { register, collectDefaultMetrics } = require("prom-client");
+// enable prom-client to expose default application metrics
+collectDefaultMetrics({
+  timeout: 10000,
+});
 
 // setup app
 const app = express();
@@ -38,11 +40,20 @@ app.use(cors(corsOptions)); // middleware to enables cors
 app.use(helmet()); // middleware which adds http headers
 app.use(bodyParser.urlencoded({ extended: false, limit: "10mb" })); // middleware which parses body
 app.use(bodyParser.json({ limit: "10mb" })); // converts body to json
+app.get("/metrics", async (req, res) => {
+  try {
+    res.set("Content-Type", register.contentType);
+    const met = await register.metrics();
+    res.send(met);
+  } catch (error) {
+    console.log("metrics route error", error);
+  }
+});
 
 // bring all routes here
 const routes = require("./routes");
 
-app.use(promMetrics);
+// app.use(promMetrics);
 app.use("/", routes);
 
 const port = process.env.PORT || 5051;
