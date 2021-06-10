9e1bf726ccb1d450228c834ce1dd199581f7d12a
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-06-10 18:08:59 +0700

    Update routes

--- index.js
+++ index.js
@@ -2,39 +2,24 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const morgan = require("morgan");
+const socketIO = require("socket.io");
 const socketRedis = require("socket.io-redis");
 const log = require("loglevel");
 const Sentry = require("@sentry/node");
 const Tracing = require("@sentry/tracing");
 
-// setup app
+// setup environment
+require("dotenv").config();
+
+const awsIdleTimeout = 60 * 1000; // AWS ALB idle timeout
+
+// setup Express app
 const app = express();
 const server = require("http").createServer(app);
 
-const awsIdleTimeout = 60 * 1000; // AWS ALB idle timeout
-const io = require("socket.io")(server, {
-  transports: ["websocket"],
-  cors: {
-    credentials: true,
-    origin: true,
-    methods: ["GET", "POST"],
-  },
-  // Let AWS ALB drop connections
-  connectTimeout: awsIdleTimeout + 5 * 1000,
-  pingTimeout: awsIdleTimeout + 5 * 1000,
-  upgradeTimeout: awsIdleTimeout + 5 * 1000,
-});
+// setup Sentry
 const redact = require("./utils/redactSentry");
 
-io.adapter(socketRedis({ host: process.env.REDIS_HOSTNAME, port: process.env.REDIS_PORT }));
-
-io.on("connection", () => {
-  log.debug("connected");
-});
-// Setup environment
-require("dotenv").config();
-
-// setup Sentry
 const isSentryConfigured = process.env.SENTRY_DSN && process.env.SENTRY_DSN.length > 0;
 if (isSentryConfigured) {
   Sentry.init({
@@ -76,13 +61,38 @@ app.use(express.urlencoded({ extended: false, limit: "10mb" })); // middleware w
 app.use(express.json({ limit: "10mb" })); // converts body to json
 
 // bring all routes here
-const routes = require("./routes")(io);
+const routes = require("./routes");
 
 app.use("/", routes);
 
+// setup socket.io
+const io = socketIO(server, {
+  transports: ["websocket"],
+  cors: {
+    credentials: true,
+    origin: true,
+    methods: ["GET", "POST"],
+  },
+  // let AWS ALB drop connections
+  connectTimeout: awsIdleTimeout + 5 * 1000,
+  pingTimeout: awsIdleTimeout + 5 * 1000,
+  upgradeTimeout: awsIdleTimeout + 5 * 1000,
+});
+
+io.adapter(socketRedis({ host: process.env.REDIS_HOSTNAME, port: process.env.REDIS_PORT }));
+io.on("connection", () => {
+  log.debug("connected");
+});
+
+// bring routes that require socket.io
+const emailAuthDataRoute = require("./routes/emailAuthData")(io);
+
+app.use("/auth", emailAuthDataRoute);
+
+// start server
 const port = process.env.PORT || 5051;
 server.listen(port, () => log.info(`Server running on port: ${port}`));
 
-// Let AWS ALB drop connections (ref https://shuheikagawa.com/blog/2019/04/25/keep-alive-timeout)
+// let AWS ALB drop connections (ref https://shuheikagawa.com/blog/2019/04/25/keep-alive-timeout)
 server.keepAliveTimeout = awsIdleTimeout + 5 * 1000;
 server.headersTimeout = awsIdleTimeout + 10 * 1000;
--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
-module.exports = (io) => {
-  const defaultRoute = require("./default");
-  const metadataRoute = require("./metadata");
-  const lockRoute = require("./lock");
-  const emailAuthDataRoute = require("./emailAuthData")(io);
+const defaultRoute = require("./default");
+const metadataRoute = require("./metadata");
+const lockRoute = require("./lock");
 
-  router.use("/", defaultRoute);
-  router.use("/", metadataRoute);
-  router.use("/", lockRoute);
-  router.use("/auth", emailAuthDataRoute);
-  return router;
-};
+router.use("/", defaultRoute);
+router.use("/", metadataRoute);
+router.use("/", lockRoute);
+
+module.exports = router;
