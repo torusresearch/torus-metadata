9a36974441ef3084e5fa15a1b971276c0cf802cd
rathishubham7 <shubhamrathi@icloud.com>
2021-07-07 17:05:44 +0530

    feat: added get/set metadata tests

--- test/api.js
+++ test/api.js
@@ -8,10 +8,19 @@
  * @requires NPM:chai-http
  */
 
+// setup for TorusStorageLayer
+global.btoa = require("btoa");
+global.atob = require("atob");
+global.fetch = require("node-fetch");
+
+const { TorusStorageLayer } = require("@tkey/storage-layer-torus");
+
 // During the test the env variable is set to test
 // Require the dev-dependencies
 const chai = require("chai");
 const chaiHttp = require("chai-http");
+const BN = require("bn.js");
+const { generatePrivate } = require("@toruslabs/eccrypto");
 require("dotenv").config();
 
 const port = 5051;
@@ -42,26 +51,42 @@ describe("API-calls", function () {
   before("One time execution for all tests", async function () {});
 
   /*
-   * Test the /GET route
+   * Test the /default route
    */
-  describe("/Homepage", function () {
+  describe("/default", function () {
     it("it should return a welcome message", async function () {
       const res = await request(server).get("/");
-      console.log("res.text is", res.text);
       assert.strictEqual(res.status, 200);
       assert.strictEqual(res.text, "Welcome to Torus Metadata");
     });
 
     it("it should return ok message", async function () {
       const res = await request(server).get("/health");
-      console.log("res.text is", res.text);
       assert.strictEqual(res.status, 200);
       assert.strictEqual(res.text, "Ok!");
     });
   });
 
+  describe.only("/metadata", function () {
+    let PRIVATE_KEY;
+    let storageLayer = new TorusStorageLayer({ hostUrl: server });
+
+    beforeEach(function () {
+      PRIVATE_KEY = new BN(generatePrivate());
+    });
+
+    it("it should be able to set/get metadata with correct validation", async function () {
+      const message = {
+        test: Math.random().toString(36).substring(7),
+      };
+
+      await storageLayer.setMetadata({ input: message, privKey: PRIVATE_KEY });
+      let data = await storageLayer.getMetadata({ privKey: PRIVATE_KEY });
+      assert.strictEqual(data.test, message.test);
+    });
+  });
+
   after(function () {
-    console.log("After block called after conducting all the unit tests");
     // done();
   });
 
