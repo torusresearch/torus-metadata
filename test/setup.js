049e4612c68d38da7cb9987f1a73f992302f014f
chaitanyapotti <chaitanya.potti@gmail.com>
2022-03-01 17:08:27 +0800

    setup quick local dev env for testing

--- test/setup.js
+++ test/setup.js
@@ -5,8 +5,8 @@ global.fetch = require("node-fetch");
 global.FormData = require("form-data");
 
 // Comment in/out the following line to include/disclude comments in terminal
-if (process.env.LOG_DEBUG === "no") {
-  console.log = function () {};
-}
+// if (process.env.LOG_DEBUG === "no") {
+//   console.log = function () {};
+// }
 
 require("dotenv").config();
